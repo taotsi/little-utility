547b6d7e3d501fa0d0ca66c536efb803d37ad2d6
taotsi <tmkf.guthrie@gmail.com>
2019-08-29 01:19:14 +0800

    butterworth high pass

--- include/little-utility/filter.hh
+++ include/little-utility/filter.hh
@@ -2,28 +2,47 @@
 #define FILTER_HH
 
 #include <cmath>
+#include <functional>
 #include "macros.hh"
 
 namespace taotsi
 {
 
-class ButterWorth
+class Filter
 {
 public:
-  ButterWorth(double wcl, double wch, double amp = 1.0)
-    : wcl_{wcl}, wch_{wch}, amp_{amp}
-  {};
-  DEFAULT_SPECIAL_FUNCTIONS(ButterWorth);
+  Filter() {};
+  DEFAULT_SPECIAL_FUNCTIONS(Filter);
 
   template<typename T>
   double operator()(T w)
   {
     auto wd = static_cast<double>(w);
-    double val = amp_ *
-      (1.0 / (sqrt(1.0 + pow(pow(2.0, wd - wch_), 2.0)))) *
-      (1.0 / (sqrt(1.0 + pow(pow(2.0, wcl_ - wd), 2.0))));
-    return val;
+    double result = 1.0;
+    for(auto &filter : filters_)
+    {
+      result *= filter(wd);
+    }
+    return result;
   }
+protected:
+  std::vector<std::function<double(double)>> filters_;
+};
+
+class ButterWorthHP : public Filter
+{
+public:
+  ButterWorthHP(double wcl, double wch, double amp = 1.0)
+    : wcl_{wcl}, wch_{wch}, amp_{amp}
+  {
+    std::function<double(double)> filter = [this](double w)
+    {
+      return 1.0 / (sqrt(1.0 + pow(pow(2.0, w - this->wch_), 2.0)));
+    };
+    filters_.push_back(filter);
+  };
+  DEFAULT_SPECIAL_FUNCTIONS(ButterWorthHP);
+
 private:
   double wcl_, wch_, amp_;
 };
@@ -36,14 +55,6 @@ public:
   {
     mean_ = (wch_ + wcl_) / 2.0;
     var_ = wch_ - mean_;
-  }
-  template<typename T>
-  double operator()(T w)
-  {
-    auto wd = static_cast<double>(w);
-    double val = amp_ *
-      exp(-pow(wd - mean_, 2.0)/(2.0*pow(var_, 2.0)));
-    return val;
   }
 
 private:
--- test/test_utility.cc
+++ test/test_utility.cc
@@ -8,79 +8,78 @@
 using namespace taotsi;
 using namespace std::chrono_literals;
 
-TEST_CASE("test")
+TEST_CASE("single filter")
 {
-  REQUIRE(1 == 1);
-}
-
-void TestMsg();
-void TestTimer();
-void TestCounter();
-void test_rand();
-void test_filter();
-
-void TestMsg()
-{
-  // int x = 42;
-  // tval(x);
-  // tmark();
-  std::vector<int> vec{1, 2, 3, 4, 5};
-  tval(vec);
-}
-void TestTimer()
-{
-  Timer t;
-  t.tStart();
-  std::this_thread::sleep_for(1234ms);
-  t.tPutNow();
-}
-
-void TestCounter()
-{
-  for (size_t i = 0; i < 50; i++)
+  SECTION("butterwort low path")
   {
-    COUNT_HERE();
-  }
-  SHOW_COUNTS();
-  for (size_t i = 0; i < 10000; i++)
-  {
-    COUNT_HERE();
+    ButterWorthHP bl{10.0, 100.0};
+    REQUIRE(bl(45.0) == 1.0);
+    REQUIRE(bl(100.0) - 0.707 < 0.1);
   }
 }
 
-void test_rand()
-{
-  // auto di = Rand::int_random_dice(0, 99);
-  // for (size_t i = 0; i < 20; i++)
-  // {
-  //   tval(di());
-  // }
-  // auto dp = Rand::probability_dice();
-  // for (size_t i = 0; i < 20; i++)
-  // {
-  //   tval(dp());
-  // }
-  int count = 0;
-  for (size_t i = 0; i < 10000; i++)
-  {
-    if(Rand::die_once(0.5))
-    {
-      count++;
-    }
-  }
-  tval(count);
-}
+// void TestMsg()
+// {
+//   // int x = 42;
+//   // tval(x);
+//   // tmark();
+//   std::vector<int> vec{1, 2, 3, 4, 5};
+//   tval(vec);
+// }
+// void TestTimer()
+// {
+//   Timer t;
+//   t.tStart();
+//   std::this_thread::sleep_for(1234ms);
+//   t.tPutNow();
+// }
 
-void test_filter()
-{
-  ButterWorth bw{10.0, 100.0};
-  for(double w=0; w<200; w+=1)
-  {
-    std::cout << bw(w) << "\n";
-  }
-  Gauss gs{10, 100};
-  for(double w=0; w<200; w+=1)
-  {
-    std::cout << gs(w) << "\n";
-  }
-}
+// void TestCounter()
+// {
+//   for (size_t i = 0; i < 50; i++)
+//   {
+//     COUNT_HERE();
+//   }
+//   SHOW_COUNTS();
+//   for (size_t i = 0; i < 10000; i++)
+//   {
+//     COUNT_HERE();
+//   }
+// }
+
+// void test_rand()
+// {
+//   // auto di = Rand::int_random_dice(0, 99);
+//   // for (size_t i = 0; i < 20; i++)
+//   // {
+//   //   tval(di());
+//   // }
+//   // auto dp = Rand::probability_dice();
+//   // for (size_t i = 0; i < 20; i++)
+//   // {
+//   //   tval(dp());
+//   // }
+//   int count = 0;
+//   for (size_t i = 0; i < 10000; i++)
+//   {
+//     if(Rand::die_once(0.5))
+//     {
+//       count++;
+//     }
+//   }
+//   tval(count);
+// }
+
+// void test_filter()
+// {
+//   ButterWorth bw{10.0, 100.0};
+//   for(double w=0; w<200; w+=1)
+//   {
+//     std::cout << bw(w) << "\n";
+//   }
+//   Gauss gs{10, 100};
+//   for(double w=0; w<200; w+=1)
+//   {
+//     std::cout << gs(w) << "\n";
+//   }
+// }
