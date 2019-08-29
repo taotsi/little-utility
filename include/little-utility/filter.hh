83de08a2a77a7d8ef8081e2e7fbf255ff250f1d6
Marvin Song <weitao.song@dorabot.com>
2019-08-29 11:30:57 +0800

    butterworth lp and hp filter

--- include/little-utility/filter.hh
+++ include/little-utility/filter.hh
@@ -44,12 +44,29 @@ public:
   {
     std::function<double(double)> filter = [amp = amp_, wc = wc_](double w)
     {
-      return amp / (sqrt(1.0 + pow(pow(2.0, w - wc), 2.0)));
+      return amp / (sqrt(1.0 + pow(pow(2.0, wc - w), 2.0)));
     };
     filters_.push_back(filter);
   };
   DEFAULT_SPECIAL_FUNCTIONS(ButterWorthHP);
 
+private:
+  double wc_, amp_;
+};
+
+class ButterWorthLP : public Filter
+{
+public:
+  ButterWorthLP(double wc, double amp = 1.0)
+    : wc_{wc}, amp_{amp}
+  {
+    std::function<double(double)> filter = [amp = amp_, wc = wc_](double w)
+    {
+      return amp / (sqrt(1.0 + pow(pow(2.0, w - wc), 2.0)));
+    };
+    filters_.push_back(filter);
+  };
+  DEFAULT_SPECIAL_FUNCTIONS(ButterWorthLP);
 private:
   double wc_, amp_;
 };
